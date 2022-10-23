5b8e599cb6a0ff6d0ea2b8f3425fbfa9a6062199
Liberti1991 <liberti1991@gmail.com>
2022-10-23 20:48:45 -0300

    ajuste

--- src/componentes/pages/Contact.jsx
+++ src/componentes/pages/Contact.jsx
@@ -4,146 +4,152 @@ import { Title } from "../layout/Title";
 
 export const Contact = () => {
   return (
-    <>
+    <Container>
       <Title>
         <h1>Contatos</h1>
       </Title>
-      <Container>
+      <div>
         {dbFooter.map((item) => {
           return (
             <a key={item.id} href={item.url} rel={item.rel} target={item.target} title={item.title} alt={item.title}>
-              <div style={{ color: `${item.className}`}}>{item.svg}</div>
+              <div style={{ color: `${item.className}` }}>{item.svg}</div>
               {item.title}
             </a>
           );
         })}
-      </Container>  
-    </>
+      </div>
+    </Container>
   );
 };
 
 const Container = styled.div`
-  padding-top: 30px;
-  width: 270px;
-  margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 20px;
-
-  a {
-    font-size: 1.2rem;
+  > div {
+    padding-top: 30px;
+    width: 270px;
+    margin: 0 auto;
     display: flex;
-    align-items: center;
+    flex-direction: column;
+    justify-content: center;
     gap: 20px;
-    border-right: 1px solid orange;
-    overflow: hidden;
-    width: 0;
-    white-space: nowrap;
-    text-decoration: none;
-    color: white;
-    position: relative;
-    transition: all 1s;
 
-    ::after {
-      content: "";
-      width: 100%;
-      height: 1px;
-      background-color: orange;
-      border-radius: 4px;
-      position: absolute;
-      left: 0;
-      bottom: 0;
-      transform: scaleX(0);
-      transform-origin: left;
-      transition: transform 0.25s ease;
-    }
+    a {
+      font-size: 1.2rem;
+      display: flex;
+      align-items: center;
+      gap: 20px;
+      border-right: 1px solid orange;
+      overflow: hidden;
+      width: 0;
+      white-space: nowrap;
+      text-decoration: none;
+      color: white;
+      position: relative;
+      transition: all 1s;
 
-    :hover::after {
-      transform: scaleX(1);
-    }
-
-    :nth-child(1) {
-      width: 11.5ch;
-      opacity: 0;
-      animation: type 1.3s steps(40, end) forwards;
-    }
-
-    :nth-child(2) {
-      width: 10.5ch;
-      opacity: 0;
-      animation: type 1.3s steps(40, end) forwards;
-      animation-delay: 1.5s;
-    }
-
-    :nth-child(3) {
-      width: 13ch;
-      opacity: 0;
-      animation: type 1.3s steps(40, end) forwards;
-      animation-delay:  3s;
-    }
-
-    :nth-child(4) {
-      width: 24ch;
-      opacity: 0;
-      animation: type 2s steps(40, end) forwards;
-      animation-delay: 4.5s;
-    }
-    :nth-child(5) {
-      width: 12.5ch;
-      opacity: 0;
-      animation: type2 1.3s steps(40, end) forwards, blink 0.7s step-end infinite alternate;
-      animation-delay: 6.5s;
-    }
-
-    @keyframes type {
-      0% {
-        width: 0;
-        opacity: 1;
+      ::after {
+        content: "";
+        width: 100%;
+        height: 1px;
+        background-color: orange;
+        border-radius: 4px;
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        transform: scaleX(0);
+        transform-origin: left;
+        transition: transform 0.25s ease;
       }
-      99% {
-        border-right: 1px solid orange;
-      }
-      100% {
-        opacity: 1;
-        border: none;
-      }
-    }
 
-    @keyframes type2 {
-      0% {
-        width: 0;
+      :hover::after {
+        transform: scaleX(1);
       }
-      1% {
-        opacity: 1;
-      }
-      100% {
-        opacity: 1;
-      }
-    }
 
-    @keyframes blink {
-      50% {
-        border-color: transparent;
+      :nth-child(1) {
+        width: 11.5ch;
+        opacity: 0;
+        animation: type 1.3s steps(40, end) forwards;
       }
-    }
 
-    div {
-      svg {
-        width: 30px;
-        height: 30px;
+      :nth-child(2) {
+        width: 10.5ch;
+        opacity: 0;
+        animation: type 1.3s steps(40, end) forwards;
+        animation-delay: 1.5s;
+      }
+
+      :nth-child(3) {
+        width: 13ch;
+        opacity: 0;
+        animation: type 1.3s steps(40, end) forwards;
+        animation-delay: 3s;
+      }
+
+      :nth-child(4) {
+        width: 24ch;
+        opacity: 0;
+        animation: type 2s steps(40, end) forwards;
+        animation-delay: 4.5s;
+      }
+      :nth-child(5) {
+        width: 12.5ch;
+        opacity: 0;
+        animation: type2 1.3s steps(40, end) forwards, blink 0.7s step-end infinite alternate;
+        animation-delay: 6.5s;
+      }
+
+      @keyframes type {
+        0% {
+          width: 0;
+          opacity: 1;
+        }
+        99% {
+          border-right: 1px solid orange;
+        }
+        100% {
+          opacity: 1;
+          border: none;
+        }
+      }
+
+      @keyframes type2 {
+        0% {
+          width: 0;
+        }
+        1% {
+          opacity: 1;
+        }
+        100% {
+          opacity: 1;
+        }
+      }
+
+      @keyframes blink {
+        50% {
+          border-color: transparent;
+        }
+      }
+
+      div {
+        svg {
+          width: 30px;
+          height: 30px;
+        }
       }
     }
   }
 
   @media screen and (min-width: 900px) {
-    padding-top: 0px;
-    padding-left: 50px;
+    > div {
+      padding-top: 0px;
+      padding-left: 50px;
+    }
   }
 
   @media screen and (max-height: 400px) {
-    height: calc(100vh - 90px);
-    overflow: auto;
-    padding-bottom: 15px;
+    > div {
+      height: calc(100vh - 90px);
+      overflow: auto;
+      padding-bottom: 15px;
+    }
   }
 `;
